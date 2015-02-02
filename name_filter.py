0c47691106a60bd55ee6c38bc638ae37fb85afea
Christine Fernsebner Eslao <eslao@fas.harvard.edu>
2015-02-02 10:54:24 -0500

    Remove extraneous lists
    Commented out separate direct order and inverted order name lists.

--- name_filter.py
+++ name_filter.py
@@ -4,15 +4,15 @@ import string
 filetext = open('list.txt', 'r')
 itemlist = []
 filterlist = []
-inverted_name_list = []
-direct_order_name_list = []
+#inverted_name_list = []
+#direct_order_name_list = []
 
 for line in filetext:
     item = line.strip().split(',')
     inverted_name = line.strip()
     direct_order_name = item[-1] + ' ' + ' '.join(item[:-1])
-    inverted_name_list += [inverted_name.strip()]
-    direct_order_name_list += [direct_order_name.strip()]
+    #inverted_name_list += [inverted_name.strip()]
+    #direct_order_name_list += [direct_order_name.strip()]
     filterlist += ["([BIB_TEXT] contains '" + inverted_name.strip() + "' or [BIB_TEXT] contains '" + direct_order_name.strip()+ "')"]
 
 filtertext = ' or '.join(filterlist)
